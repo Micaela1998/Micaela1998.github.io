8e047e691a47500cbd28818176854d34913ed7a3
Micaela1998 <126741020+Micaela1998@users.noreply.github.com>
2023-03-08 04:08:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var coll = document.getElementsByClassName("collapsible");
   }
 
   function myFunction() {
-    location.replace("https://github.com/Micaela1998/Micaela1998.github.io/blob/main/curriculum.html")
+    location.replace("https://github.com/Micaela1998/Micaela1998.github.io/blob/main/curriculum/index.html")
   }
 
   function myFunction2() {
